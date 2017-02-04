c3ee92c399a2338ed2897b5549d29e170608508a
tmpbook <nauy2011@126.com>
2017-02-04 23:35:08 +0800

    config django static loader

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Add for vuejs
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/dist/static"),
+]
